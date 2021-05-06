be613bf7b09b1ff3717b1aec2d5d2a0eacba4242
jonkarrer <karrerjon@gmail.com>
2021-05-06 09:59:06 -0500

    pricing

--- components/support_pages/List.tsx
+++ components/support_pages/List.tsx
@@ -3,11 +3,11 @@ interface IList {
   title: string;
 }
 const List = ({ children, title }: IList) => (
-  <div className="grid justify-items-center gap-5 sm:w-36 md:w-40 lg:w-52 xl:w-64">
-    <h2 className="grid place-content-center text-8vw w-3/4 sm:text-lg lg:text-2xl xl:text-3xl sm:h-10 sm:w-28 md:w-32 lg:w-40 xl:w-5/6 xl:h-12 border-blue-border border-solid border-1px rounded-sm">
+  <div className="grid justify-items-center gap-6 sm:w-36 md:w-40 lg:w-52 xl:w-64">
+    <h2 className="grid place-content-center text-8vw w-3/4 sm:text-lg lg:text-2xl xl:text-3xl sm:h-10 sm:w-28 md:w-32 lg:w-40 xl:w-5/6 xl:h-12 border-blue-border border-solid border-1px rounded-md">
       {title}
     </h2>
-    <ul className="list-disc text-mobile-p sm:text-sm lg:text-lg font-bold space-y-4">
+    <ul className="list-disc text-mobile-p sm:text-sm lg:text-lg font-bold space-y-5">
       {children}
     </ul>
   </div>
--- pages/support/pricing.tsx
+++ pages/support/pricing.tsx
@@ -1,8 +1,21 @@
 import Gradient from "@/components/support_pages/Gradient";
-
+import Wrapper from "@/components/support_pages/Wrapper";
 const Pricing = () => (
   <div>
     <Gradient title="Pricing" />
+    <Wrapper display="grid">
+      <div className="grid gap-4 lg:gap-12 place-content-center w-full">
+        <h1 className="text-center font-bold text-mobile-h py-8 sm:py-0 sm:text-4xl lg:text-5xl ">
+          Compare Plans
+        </h1>
+        <div className="flex justify-center items-center shadow-supp-boxes rounded-full h-12 w">
+          <div className="xl:w-96 h-full rounded-full flex justify-center items-center text-white bg-blue-border">
+            Monthly
+          </div>
+          <div className="xl:w-96 rounded-full flex justify-center">Annual</div>
+        </div>
+      </div>
+    </Wrapper>
   </div>
 );
 
