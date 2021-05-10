c8de1dd452a4a0e9015e1e1ba1e240bab31e7293
jonkarrer <karrerjon@gmail.com>
2021-05-10 13:19:35 -0500

    gradient and wrapper changes

---
+++ components/support_pages/Block.tsx
@@ -0,0 +1,17 @@
+interface IBlock {
+  q: string;
+  year: string;
+}
+
+const Block = ({ q, year }: IBlock) => (
+  <div className="h-24 w-72 lg:h-28 lg:w-96 text-center grid place-content-center space-y-3 bg-white border-blue-border border-solid border-1px transform translate-y-1/2 absolute rounded">
+    <h2 className="text-blue-border font-semibold underline">
+      {q} {year}
+    </h2>
+    <div className="border-blue-border border-solid border-1px text-blue-border w-60 lg:w-80 rounded">
+      0%
+    </div>
+  </div>
+);
+
+export default Block;
--- pages/support/roadmap.tsx
+++ pages/support/roadmap.tsx
@@ -1,6 +1,6 @@
 import Gradient from "@/components/support_pages/Gradient";
 import Wrapper from "@/components/support_pages/Wrapper";
-
+import Block from "@/components/support_pages/Block";
 const RoadMap = () => (
   <div>
     <Gradient title="Roadmap" />
@@ -12,7 +12,32 @@ const RoadMap = () => (
         </h1>
       </div>
     </Wrapper>
-    <div className="bg-blue-400 h-1000px w-full mt-12"></div>
+    <div className="pb-32 w-full max-w-screen-2xl m-auto mt-10 lg:mt-20 grid grid-cols-2">
+      <div className=" relative border-r-2 h-1150px border-blue-border border-solid">
+        <div className="sm:h-8 sm:w-8 lg:h-10 lg:w-10 bg-blue-border top-0 right-0 absolute transform translate-x-1/2 rounded"></div>
+        <div className="sm:h-8 sm:w-8 lg:h-10 lg:w-10 bg-blue-border bottom-0 right-0 absolute transform translate-x-1/2 rounded"></div>
+        <div className="h-36 border-blue-border border-solid border-b-1px relative flex items-end sm:ml-6% md:ml-12% lg:ml-20%">
+          <Block q="Q2" year="2021" />
+        </div>
+        <div className="h-80 border-blue-border border-solid border-b-1px relative flex items-end sm:ml-6% md:ml-12% lg:ml-20%">
+          <Block q="Q4" year="2021" />
+        </div>
+        <div className="h-80 border-blue-border border-solid border-b-1px relative flex items-end sm:ml-6% md:ml-12% lg:ml-20%">
+          <Block q="Q2" year="2022" />
+        </div>
+      </div>
+      <div className="border-l-2 h-1150px border-blue-border border-solid">
+        <div className="h-80 border-blue-border border-solid border-b-1px relative flex items-end justify-end sm:mr-6% md:mr-12% lg:mr-20%">
+          <Block q="Q3" year="2021" />
+        </div>
+        <div className="h-80 border-blue-border border-solid border-b-1px relative flex items-end justify-end sm:mr-6% md:mr-12% lg:mr-20%">
+          <Block q="Q1" year="2022" />
+        </div>
+        <div className="h-80 border-blue-border border-solid border-b-1px relative flex items-end justify-end sm:mr-6% md:mr-12% lg:mr-20%">
+          <Block q="Q3" year="2022" />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
