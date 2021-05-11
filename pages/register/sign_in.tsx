9c35914269c62b80a01041717d1d7610daa27564
jonkarrer <karrerjon@gmail.com>
2021-05-11 16:40:31 -0500

    sign in form done

--- pages/register/sign_in.tsx
+++ pages/register/sign_in.tsx
@@ -1,34 +1,55 @@
-import React from "react";
+import Link from "next/link";
 
 const SignIn = () => {
   return (
-    <div className="grid grid-cols-2">
-      <div id="home_page-one-gradient" className="h-screen"></div>
+    <div className="h-screen grid sm:grid-cols-2 place-content-center">
+      <div id="home_page-one-gradient" className="h-screen hidden sm:block">
+        <img src="/logo.svg" alt="logo" className="m-5 w-16" />
+      </div>
       <div className="grid place-content-center">
-        <h1 className="text-3xl py-8">Welcome to Market Maker Lite</h1>
-        <form action="Get" className="grid space-y-8 text-dk-grey">
-          <input
-            type="text"
-            className=" bg-lt-grey h-16 px-3 font-semibold"
-            placeholder="SIGN IN WITH GOOGLE"
-          />
+        <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl py-8 text-center sm:mx-15% xl:mx-0">
+          Welcome to Market Maker Lite
+        </h1>
+        <form
+          action="Get"
+          className="grid space-y-8 text-dk-grey sm:mx-8 xl:mx-0"
+        >
+          <div className="flex h-12 lg:h-auto">
+            <img
+              src="/google.svg"
+              alt="google icon"
+              className="w-12 lg:w-auto flex justify-center items-center bg-dk-grey px-2"
+            />
+            <input
+              type="text"
+              className=" bg-lt-grey h-12 lg:h-16 px-3 font-semibold w-full"
+              placeholder="SIGN IN WITH GOOGLE"
+            />
+          </div>
           <h2 className="text-center">or</h2>
           <input
             type="email"
             placeholder="Email address"
-            className="bg-lt-grey h-16 px-3"
+            className="bg-lt-grey h-12 lg:h-16 px-3"
           />
           <input
             type="password"
             placeholder="Password"
-            className="bg-lt-grey h-16 px-3"
+            className="bg-lt-grey h-12 lg:h-16 px-3"
           />
-          <a className="text-blue-border font-bold">Forgot your password?</a>
-          <div className="flex justify-between items-center">
-            <button className="text-white bg-blue-border font-bold w-52 h-14">
+          <Link href="/forgot">
+            <a className="text-blue-border font-bold">Forgot your password?</a>
+          </Link>
+          <div className="flex flex-col space-y-8 lg:space-y-0 lg:flex-row justify-between items-center">
+            <button
+              type="submit"
+              className="text-white bg-blue-border font-bold w-52 h-12 lg:h-14"
+            >
               Sign In
             </button>
-            <h3>Not a member? Sign Up</h3>
+            <Link href="/register/sign_up">
+              <h3 className="cursor-pointer">Not a member? Sign Up</h3>
+            </Link>
           </div>
         </form>
       </div>
