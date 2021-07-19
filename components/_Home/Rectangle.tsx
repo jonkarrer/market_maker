ee3127b8acbc0a05628fa960db34c4e93320b34c
jonkarrer <karrerjon@gmail.com>
2021-07-19 10:19:25 -0500

    fonts

--- components/_Home/Rectangle.tsx
+++ components/_Home/Rectangle.tsx
@@ -9,12 +9,12 @@ const Rectangle = ({ title, info, button, image }: IRectangle) => (
   <section>
     <div className="wrapper border space-y-8 p-5 flex flex-col items-center md:flex-row md:space-y-0 lg:p-8">
       <article className="grid gap-3 md:gap-4 2xl:gap-6">
-        <h4>{title}</h4>
+        <h3>{title}</h3>
         <p>{info}</p>
         <p className="cursor-pointer">{button}</p>
       </article>
       <img
-        className="z-10 2xl:w-190 3xl:w-220"
+        className="z-10 lg:w-160 2xl:w-190 3xl:w-220"
         src={image}
         alt="rectangle infographic"
       />
