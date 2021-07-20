9d8f35d885d7821820b56573003cd648684448f3
jonkarrer <karrerjon@gmail.com>
2021-07-20 09:49:53 -0500

    version one complete

--- components/_Home/Splash.tsx
+++ components/_Home/Splash.tsx
@@ -8,7 +8,7 @@ const Splash = (): JSX.Element => (
           <h1 className="sm:w-96 md:w-120 lg:w-140 2xl:w-160 lg:leading-tight 3xl:w-200">
             Make the market you deserve
           </h1>
-          <span className="text-xl md:text-xl lg:text-2xl 2xl:text-3xl 3xl:text-4xl md:w-96 lg:w-110 2xl:w-130 3xl:w-150">
+          <span className="text-xl max-w-sm md:max-w-none md:text-xl lg:text-2xl 2xl:text-3xl 3xl:text-4xl md:w-96 lg:w-110 2xl:w-130 3xl:w-150">
             Market alerts, statistics and analytics. Delivered through an
             innovative interface. Made for retail investors.
           </span>
@@ -17,7 +17,7 @@ const Splash = (): JSX.Element => (
       </div>
       <a href="#features">
         <img
-          className="absolute bottom-0 right-0 left-0 m-auto cursor-pointer"
+          className="absolute bottom-0 right-0 left-0 m-auto cursor-pointer z-10"
           src="/Home/splash_arrow.svg"
           alt="downward arrow"
         />
