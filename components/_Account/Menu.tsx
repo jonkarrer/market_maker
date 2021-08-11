6f1e23de3dd8c8ffcca7f974f66bf27951f0f815
jonkarrer <karrerjon@gmail.com>
2021-08-10 20:04:08 -0500

    mobile subscriptions page work

--- components/_Account/Menu.tsx
+++ components/_Account/Menu.tsx
@@ -4,8 +4,8 @@ const Menu = ({ selection }: { selection: any }) => {
   const [openAccount, setOpenAccount] = useState(true);
   const [openBilling, setOpenBilling] = useState(true);
   return (
-    <div className="text-gray-600 dark:text-white flex md:max-w-max md:block lg:flex-1 lg:pr-12 md:border-r">
-      <ul className="list-none grid md:block md:space-y-3 relative">
+    <div className="text-gray-600 dark:text-white flex md:max-w-max md:grid md:gap-12 lg:flex-1 lg:pr-12 md:border-r">
+      <ul className="list-none grid md:block md:space-y-4 relative">
         <h6
           className="font-bold text-2xl pr-10 md:pr-0"
           onClick={() => setOpenAccount(!openAccount)}
@@ -13,8 +13,8 @@ const Menu = ({ selection }: { selection: any }) => {
           Account
         </h6>
         <div
-          className={`absolute -bottom-36 bg-white grid gap-4 z-10 w-32 pl-2 py-3 border md:w-auto md:bg-none  ${
-            openAccount ? "hidden" : "block"
+          className={`absolute -bottom-36 bg-white grid gap-4 z-10 w-32 pl-2 py-3 border md:static md:w-auto md:border-none md:bg-none md:p-0 md:space-y-5 ${
+            openAccount ? "hidden md:block" : "block"
           }`}
         >
           <li
@@ -43,7 +43,7 @@ const Menu = ({ selection }: { selection: any }) => {
           </li>
         </div>
       </ul>
-      <ul className="list-none grid md:block md:space-y-3 relative">
+      <ul className="list-none grid md:block md:space-y-4 relative">
         <h6
           className="font-bold text-2xl"
           onClick={() => setOpenBilling(!openBilling)}
@@ -51,8 +51,8 @@ const Menu = ({ selection }: { selection: any }) => {
           Billing
         </h6>
         <div
-          className={`absolute -bottom-16 grid gap-4 z-10 w-32 pl-2 py-3 border bg-white md:w-auto md:bg-none  ${
-            openBilling ? "hidden" : "block"
+          className={`absolute -bottom-16 grid gap-4 z-10 w-32 pl-2 py-3 border bg-white md:static md:border-none md:w-auto md:bg-none md:p-0 ${
+            openBilling ? "hidden md:block" : "block"
           }`}
         >
           <li onClick={() => selection(4)}>Subscriptions</li>
